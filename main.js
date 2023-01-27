8ab4fc9423fa1267372b3e1e97014d535fff578e
leuldawit <97153381+leuldawit@users.noreply.github.com>
2023-01-27 20:39:48 +0300

    naming fix and js added

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function homebtnListener(){
+    document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+
+}
+function aboutbtnListener(){
+    
+    
+}
+
+function projectbtnListener(){
+    
+}
