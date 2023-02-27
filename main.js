0242d934467ed9085c811eb6ca29ad05c4c502e6
leuldawit <97153381+leuldawit@users.noreply.github.com>
2023-02-27 21:34:09 +0300

    some visual changes and GitHub graph added

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 
 
-function homebtnListener(){
-    document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-  
+    
+  function changeTox(x) {
+    var menu=document.getElementById("hamburger");
+    var items=document.getElementById("nav-items");
+    x.classList.toggle("change");
+    
+      if(menu.style.display=='none' ) {
+        menu.style.display="block" ;
+        items.classList.toggle("change");
+      } 
+      else { 
+        items.classList.toggle("change"); 
+      }
+    }
 
-}
+    function scrolltohome(){
+      document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
+  
+  }
+  
 function aboutbtnListener(){
     
     
@@ -13,4 +29,12 @@ function aboutbtnListener(){
 
 function projectbtnListener(){
     
+}
+
+    function scrolltohome(){
+      document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
+  
+  
 }
